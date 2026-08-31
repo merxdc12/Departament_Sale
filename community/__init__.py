db7ea2e87317c591a7dab68696b1f676fb3048b1
merxdc12 <92225563+merxdc12@users.noreply.github.com>
2026-08-31 20:45:48 +0200

    feat(community): export content strategy engine

--- community/__init__.py
+++ community/__init__.py
@@ -1,5 +1,6 @@
 """Rule-aware community marketing intelligence."""
 
+from .content_strategy import ContentStrategy, build_content_strategy
 from .discovery import RankedCommunity, SubredditCandidate, rank_subreddits
 from .intelligence import RedditIntelligenceInput, build_reddit_opportunity
 from .models import CommunityOpportunity, CommunityPolicy, CommunitySignals
@@ -11,6 +12,7 @@ __all__ = [
     "CommunityOpportunity",
     "CommunityPolicy",
     "CommunitySignals",
+    "ContentStrategy",
     "DiscussionSample",
     "PainMiningResult",
     "PainSignal",
@@ -20,6 +22,7 @@ __all__ = [
     "SubredditCandidate",
     "analyze_reddit_opportunity",
     "analyze_rule_evidence",
+    "build_content_strategy",
     "build_reddit_opportunity",
     "mine_pains",
     "rank_subreddits",
