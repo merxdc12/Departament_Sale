7baea5361742626dfec6f59e16d00afb06aee929
merxdc12 <92225563+merxdc12@users.noreply.github.com>
2026-09-01 06:20:46 +0200

    feat: add workflow coordinator exports

---
+++ workflow/__init__.py
@@ -0,0 +1,3 @@
+from .coordinator import OpportunityWorkflowCoordinator, OpportunityWorkflowInput, OpportunityWorkflowResult
+
+__all__ = ["OpportunityWorkflowCoordinator", "OpportunityWorkflowInput", "OpportunityWorkflowResult"]
